8a6f0c5ed4fd68c3eaee778b51454817d2caf21f
srikanth6066 <vinay.srikanth1@gmail.com>
2023-11-30 12:06:48 +0530

    tox, flake8 added

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,3 +1,21 @@
+import pytest
+
+class NotInRange(Exception):
+    def __init__(self,message="value not in range- by oracle india"):
+        self.message=message
+        super().__init__(self.message)   
+
+
+def test_generic1():
+    a = 15
+    if a not in range(10,20):
+            raise NotInRange
+
+def test_generic2():
+    a = 5
+    with pytest.raises(NotInRange):
+        if a not in range(10,20):
+            raise NotInRange        
 
 def test_generic():
     a = 30
